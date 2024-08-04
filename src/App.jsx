b7bed85bb4443e5bcb181795f602c432f48742e6
migel1976 <migel1976@gmail.com>
2024-08-04 12:01:37 +0300

    delete logic without redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import './App.css'
 
 const App = () => {
   const [todos, setTodos] = useState([])
@@ -13,27 +14,31 @@ const App = () => {
         {
           id: new Date().toISOString(),
           text,
-          completed: false
+          completed: true
         }
       ])
       setText('')
     }
   }
 
+  const removeTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
   return (
-    <>
+    <div className='App'>
       <label>
         <input type='text' value={text} onChange={(e) => setText(e.target.value)} />
         <button onClick={addTodo}>Add todo</button>
       </label>
       <ul>
         {todos.map(todo => <li key={todo.id}>
-          <input type='checkbox' />
+          <input type='checkbox' checked={todo.completed} />
           <span>{todo.text}</span>
-          <span style={{ color: 'red' }}>&times;</span>
+          <span style={{ color: 'red' }} onClick={() => removeTodo(todo.id)} >&times;</span>
         </li>)}
       </ul>
-    </>
+    </div>
   )
 }
 export default App
